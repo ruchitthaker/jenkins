1de87981410a94e793bda6e26a2015584a8664d0
ruchitthaker <ruchitthaker2001@gmail.com>
2021-08-24 15:55:46 +0530

    added files2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+	System.out.println("NAME: RUCHIT THAKER");
+	System.out.println("BATCH: T24  ROLL NUMBER: 134");  
     }
 }
